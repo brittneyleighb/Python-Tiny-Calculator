da659133001bf9259050a0d56f91b2440ce33538
Brittney Leigh Banning <117863391+brittneyleighb@users.noreply.github.com>
2023-11-17 23:00:46 +0800

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,11 @@
+a = input("first number? ")
+b = input("first number? ")
+print(int(a) + int(b))
+
+a = input("first number? ")
+b = input("first number? ")
+print(int(a) * int(b))
+
+a = input("first number? ")
+b = input("first number? ")
+print(int(a) - int(b))
